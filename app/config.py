2e3a31732fff89e89f09f767998167802f2060d4
blackat. <nguoiaoden111@gmail.com>
2025-04-14 23:48:32 +0700

    finish ui

--- app/config.py
+++ app/config.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
     # Security settings
     JWT_SECRET_KEY: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
     JWT_ALGORITHM: str = "HS256"
-    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 180
     
     # CORS settings
     CORS_ALLOW_ORIGINS_STR: str = '["http://localhost", "http://localhost:8000", "http://127.0.0.1", "http://127.0.0.1:8000"]'
